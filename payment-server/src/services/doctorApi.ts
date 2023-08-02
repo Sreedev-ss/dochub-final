9ce7acba4594ec72785c9abcf42e2a6671a637cb
Sreedev-ss <sreedevkply2004@gmail.com>
2023-08-03 01:37:17 +0530

    stripe update

--- payment-server/src/services/doctorApi.ts
+++ payment-server/src/services/doctorApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 class DoctorApi{
     async updatePaymentStatus(paymentIntent:string,appointmentId:string){
-        const response = await axios.put(`http://localhost:8000/doc/doctor/update-payment-status`,{paymentIntent,appointmentId})
+        const response = await axios.put(`https://sreedev.live/doctor/update-payment-status`,{paymentIntent,appointmentId})
         return response
     }
 }
